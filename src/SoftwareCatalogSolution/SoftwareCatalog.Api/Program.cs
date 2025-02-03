26362a8aa6aa5dca9262a89f495e511aa8da00c9
Adam W Aidja <adamwaidja@gmail.com>
2025-02-03 14:53:51 -0500

    Initial

---
+++ src/SoftwareCatalogSolution/SoftwareCatalog.Api/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//ABOVE THIS LINE IS CONFIGURING THE INTERNALS OF THE API PROJECT
+
+var app = builder.Build(); //THE LINE IN THE SAND
+
+//EVERYTHING AFTER THIS LINE IS CONFIGUTING HOW THE WEB SERVER HANDLES INCOMING REQUESTS/RESPONSES
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.Run();
