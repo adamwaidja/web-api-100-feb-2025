eef9495564982b02b74857e6c7f4e94b8e7a5955
Adam W Aidja <adamwaidja@gmail.com>
2025-02-05 13:50:34 -0500

    lunch break update

--- src/SoftwareCatalogSolution/SoftwareCatalog.Tests/Catalog/AddingAnItem.cs
+++ src/SoftwareCatalogSolution/SoftwareCatalog.Tests/Catalog/AddingAnItem.cs
@@ -1,24 +1,27 @@
-﻿
-using Alba;
-using SoftwareCatalog.Api.Catalog;
+﻿using SoftwareCatalog.Api.Catalog;
 
 namespace SoftwareCatalog.Tests.Catalog;
-public class AddingAnItem
+public class AddingAnItem(CatalogSystemTestFixture fixture) : IClassFixture<CatalogSystemTestFixture>
 {
-    [Fact]
-    public async Task CanAddAnItem()
+
+    [Theory]
+    [InlineData("Visual Studio Code", "microsoft", CatalogItemLicenceTypes.OpenSource)]
+    [InlineData("Visual Studio", "microsoft", CatalogItemLicenceTypes.Paid)]
+    [InlineData("Bob", "microsoft", CatalogItemLicenceTypes.Free)]
+    [InlineData("Destiny 2", "bungie", CatalogItemLicenceTypes.Paid)]
+    public async Task CanAddAnItem(string name, string vendor, CatalogItemLicenceTypes license)
     {
-        var host = await AlbaHost.For<Program>();
+
 
 
         var itemToPost = new CatalogItemRequestModel
         {
-            Name = "Visual Studio Code"
+            Name = name
         };
-
-        var response = await host.Scenario(api =>
+        var resource = $"/vendors/{vendor.ToLower()}/{license.ToString().ToLower()}";
+        var response = await fixture.Host.Scenario(api =>
         {
-            api.Post.Json(itemToPost).ToUrl("/vendors/microsoft/opensource");
+            api.Post.Json(itemToPost).ToUrl(resource);
             api.StatusCodeShouldBe(201);
         });
 
@@ -27,7 +30,7 @@ public class AddingAnItem
 
         //   Assert.Equal(expectedResponse, body);
 
-        var getResponse = await host.Scenario(api =>
+        var getResponse = await fixture.Host.Scenario(api =>
         {
             api.Get.Url($"/catalog/{responseFromThePost.Id}");
         });
@@ -38,4 +41,6 @@ public class AddingAnItem
         Assert.Equal(responseFromThePost, responseFromGet);
 
     }
+
+
 }
